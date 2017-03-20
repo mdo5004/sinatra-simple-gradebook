53fc5c829469805e9afdea189120eacf92f61692
Michael O'Connell <michaeldavidoconnell@gmail.com>
2017-03-20 15:36:56 -0400

    Simple navigation in layout

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,27 @@
 class ApplicationController < Sinatra::Base
-   configure do
-       #set :public_folder, 'public'
+    configure do
+        #set :public_folder, 'public'
         set :views, 'app/views'
         enable :sessions
         set :session_secret, "iknowhowtoplaytheblackkeys"
-   end
-    
+    end
+
+    helpers do
+        def logged_in?
+            !session[:id].nil?
+        end
+
+        def current_user
+            session[:id] 
+        end
+        
+        def current_page
+            session[:page]
+        end
+    end
     get '/' do
+        session[:page]='home'
         erb :index
     end
-    
+
 end
