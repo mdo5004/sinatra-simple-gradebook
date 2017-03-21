b1cd73177ddc3ab3e96a5e3818fe0c58f25cd765
Michael O'Connell <michaeldavidoconnell@gmail.com>
2017-03-20 21:03:41 -0400

    Basic CRUD setup

--- app/controllers/student_controller.rb
+++ app/controllers/student_controller.rb
@@ -1,17 +1,44 @@
 class StudentController < ApplicationController
-    
+
     get "/students/:id/assignments" do
-        @student = Student.find(params[:id])
-        @assignments = @student.student_assignments
-        erb :"students/student"
+        if logged_in?
+            @student = Student.find(params[:id])
+            @assignments = @student.student_assignments
+            erb :"students/student"
+        else
+            redirect "/login" 
+        end
     end
-    
+
     get "/students/:id/edit" do
+        if logged_in?
+            erb :"students/edit"
+        else
+            redirect "/login" 
+        end
     end
-    
+
     get "/students/new" do
+        if logged_in?
+            erb :"students/new"
+        else
+            redirect "/login" 
+        end
     end
-    
-    post "/students/new"
-    
+
+    post "/students/new" do
+        @student = Student.create(params[:student])
+        redirect "/students"
+    end
+
+    get "/students" do
+        if logged_in?
+            @students = Student.all
+            erb :"students/index"
+        else
+            redirect "/login" 
+        end
+
+    end
+
 end
