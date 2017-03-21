95a12532458a84f7a5f3ef9729b1c6c4b76868d0
Michael O'Connell <michaeldavidoconnell@gmail.com>
2017-03-20 21:59:28 -0400

    Student detail view

--- app/controllers/student_controller.rb
+++ app/controllers/student_controller.rb
@@ -1,29 +1,30 @@
 class StudentController < ApplicationController
 
-    get "/students/:id/assignments" do
-#        if logged_in?
+    get "/students/:id/edit" do
+        if logged_in?
+            erb :"students/edit"
+        else
+            redirect "/login" 
+        end
+    end
+    
+    get "/students/:id" do
+        if logged_in?
             @student = Student.find(params[:id])
             @assignments = @student.student_assignments
             erb :"students/student"
-#        else
-#            redirect "/login" 
-#        end
-    end
+        else
+            redirect "/login" 
+        end
 
-    get "/students/:id/edit" do
-#        if logged_in?
-            erb :"students/edit"
-#        else
-#            redirect "/login" 
-#        end
     end
-
+    
     get "/students/new" do
-#        if logged_in?
+        if logged_in?
             erb :"students/new"
-#        else
-#            redirect "/login" 
-#        end
+        else
+            redirect "/login" 
+        end
     end
 
     post "/students/new" do
@@ -32,12 +33,12 @@ class StudentController < ApplicationController
     end
 
     get "/students" do
-#        if logged_in?
+        if logged_in?
             @students = Student.all
             erb :"students/index"
-#        else
-#            redirect "/login" 
-#        end
+        else
+            redirect "/login" 
+        end
 
     end
 
