49e644567bd4c887a45e19d664a831bae60b6495
Michael O'Connell <michaeldavidoconnell@gmail.com>
2017-03-22 14:05:40 -0400

    New assignments get assigned to all students in a klass

--- app/models/assignment.rb
+++ app/models/assignment.rb
@@ -1,6 +1,18 @@
+require 'pry'
 class Assignment < ActiveRecord::Base
     has_many :student_assignments
     has_many :students, through: :student_assignments
-    
+
     belongs_to :klass
+
+    after_create :assign_to_students
+
+    
+    def assign_to_students
+        self.klass.students.each do |student|
+            if student.student_assignments.where(assignment_id: self.id).empty?
+                self.student_assignments.create(student_id: student.id)
+            end
+        end
+    end
 end
