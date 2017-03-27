cd76895a1a665bae71a9afa0eaca952bab5da2c0
Michael O'Connell <michaeldavidoconnell@gmail.com>
2017-03-27 13:35:31 -0400

    Edit student submits a put request

--- app/controllers/student_controller.rb
+++ app/controllers/student_controller.rb
@@ -1,48 +1,4 @@
 class StudentController < ApplicationController
-
-    get "/students/:id/edit" do
-        if logged_in? 
-            #all teachers can edit students?
-            @student = Student.find(params[:id])
-            erb :"students/edit"
-        else
-            redirect "/login" 
-        end
-    end
-    get "/students/new" do
-        if logged_in?
-            erb :"students/new"
-        else
-            redirect "/login" 
-        end
-    end
-    get "/students/:id" do
-        if logged_in?
-            @student = Student.find(params[:id])
-            @assignments = @student.student_assignments
-            erb :"students/student"
-        else
-            redirect "/login" 
-        end
-
-    end
-
-
-    post "/students/:id/edit" do
-        Student.find(params[:id]).update(params[:student])
-        redirect "/students/#{params[:id]}"
-    end
-    post "/students/new" do
-        if params[:student][:name] != ''
-            @student = Student.create(params[:student])
-            flash[:success] = "Student created!"
-            redirect "/students"
-        else 
-            flash[:error]="Your student must have a name"
-            redirect "/students/new"
-        end
-    end
-
     get "/students" do
         if logged_in?
             @students = Student.all
@@ -52,5 +8,57 @@ class StudentController < ApplicationController
         end
 
     end
+    get "/students/new" do
+        if logged_in?
+            erb :"students/new"
+        else
+            redirect "/login" 
+        end
+    end
+    get "/students/:id/edit" do
+        if logged_in? 
+            #all teachers can edit students?
+            @student = Student.find_by(id:params[:id])
+            erb :"students/edit"
+        else
+            redirect "/login" 
+        end
+    end
+    
+    get "/students/:id" do
+        if logged_in?
+            @student = Student.find_by(id:params[:id])
+            @assignments = @student.student_assignments
+            erb :"students/student"
+        else
+            redirect "/login" 
+        end
+
+    end
+    
+
+    post "/students/new" do
+        @student = Student.new(params[:student])
+        if @student.save
+            flash[:success] = "Student created!"
+            redirect "/students"
+        else 
+            flash[:error]="Your student must have a name"
+            redirect "/students/new"
+        end
+    end
+    
+    put "/students/:id/edit" do
+        @student = Student.find_by(id:params[:id])
+        if @student.update(params[:student])
+            flash[:success] = "Student updated"
+        else
+            flash[:error] = @student.errors.full_messages.join(", ")
+        end
+        redirect "/students/#{params[:id]}"
+    end
+    
+
+    
 
 end
