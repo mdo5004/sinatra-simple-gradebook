77ca4677370550fbc4a2be2fe791a90f8f652036
Michael O'Connell <michaeldavidoconnell@gmail.com>
2017-06-15 14:09:44 -0400

    postgres

--- db/schema.rb
+++ db/schema.rb
@@ -10,17 +10,48 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170321223145) do
+ActiveRecord::Schema.define(version: 20170605125011) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
 
   create_table "assignments", force: :cascade do |t|
     t.string  "name"
     t.integer "klass_id"
   end
 
+  create_table "items", force: :cascade do |t|
+    t.string   "name"
+    t.string   "classification"
+    t.string   "manufacturer"
+    t.string   "make"
+    t.string   "model"
+    t.integer  "year"
+    t.string   "link"
+    t.datetime "created_at",     null: false
+    t.datetime "updated_at",     null: false
+  end
+
   create_table "klasses", force: :cascade do |t|
     t.string  "description"
     t.integer "teacher_id"
     t.string  "name"
+  end
+
+  create_table "rig_items", force: :cascade do |t|
+    t.integer  "rig_id"
+    t.integer  "item_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "rigs", force: :cascade do |t|
+    t.string   "name"
+    t.integer  "user_id"
+    t.string   "venue"
+    t.string   "use"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "student_assignments", force: :cascade do |t|
@@ -47,6 +78,19 @@ ActiveRecord::Schema.define(version: 20170321223145) do
     t.string "last_name"
     t.string "email"
     t.string "password_digest"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "location"
+    t.string   "title"
+    t.string   "email"
+    t.string   "password_digest"
+    t.datetime "created_at",                  null: false
+    t.datetime "updated_at",                  null: false
+    t.integer  "role",            default: 0
+    t.bigint   "uid"
+    t.string   "first_name"
+    t.string   "last_name"
   end
 
 end
