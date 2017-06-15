992def6d5be90699e3eb523c3cf3193fde212a49
Michael O'Connell <michaeldavidoconnell@gmail.com>
2017-06-15 14:02:04 -0400

    Assignments form started

--- app/controllers/assignment_controller.rb
+++ app/controllers/assignment_controller.rb
@@ -1,5 +1,17 @@
 class AssignmentController < ApplicationController
     get "/classes/:id/assignments/new" do
-        "Assignments feature coming soon"
+        @klass = Klass.find_by(id: params[:id])
+        binding.pry
+        erb :'assignments/new'
+    end
+
+    post "/classes/:id/assignments/new" do
+        klass = Klass.find(params[:id])
+        @assignment = klass.assignments.create(params[:name])
+        if @assignment.save
+            redirect to("/classes/#{params[:id]}")
+        else
+            erb :'assignments/new'
+        end
     end
 end
